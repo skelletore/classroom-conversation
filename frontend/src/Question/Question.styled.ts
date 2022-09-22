49d9f47cdeb051d8685b4291723dc12974c7e80a
netr0m <netr0m@pm.me>
2022-09-22 20:36:02 +0200

    styles: redo layout grid

--- frontend/src/Question/Question.styled.ts
+++ frontend/src/Question/Question.styled.ts
@@ -2,47 +2,142 @@ import styled from 'styled-components'
 
 import { calculateResponsiveSize } from './../helpers'
 
-export const StyledQuestion = styled.div`
-  min-height: 100%;
-  width: 100%;
+export const Convo = styled.div`
+  height: -webkit-fill-available;
+  
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-export const StyledAnswer = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  margin: 5%;
-
-  h2 {
-    width: 50%;
-    font-family: 'Gloria Hallelujah', cursive;
+  .container {
+    height: -webkit-fill-available;
+    display: grid;
+    grid-template-rows: 20% 40% 20% 20%;
+    grid-auto-columns: 1fr;
+    grid-auto-rows: 1fr;
+    gap: 0px 0px;
+    grid-auto-flow: row;
   }
 
-  .teacher {
-    padding-left: 10%;
-    padding-right: 2%;
-    text-align: left;
+  .conversation {
+    margin-top: 5%;
+    margin-left: 5%;
+    margin-right: 5%;
+    display: grid;
+    grid-template-columns: 50% 50%;
+    grid-template-rows: 100%;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+      "question answers";
+    grid-area: 1 / 1 / 2 / 2;
+
+    h2 {
+      font-family: 'Gloria Hallelujah', cursive;
+    }
   }
 
-  .student {
-    padding-left: 2%;
-    padding-right: 10%;
+  .question {
+    grid-area: question;
+  }
+
+  .answers {
     text-align: right;
-  }
-`
+    grid-area: answers;
 
-export const StyledIllustration = styled.img`
-  align-items: center;
-  text-align: center;
-  max-width: 25%;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
+    .linkedResponse {
+      margin-top: 50px;
+    }
+  }
+
+  .media {
+    display: grid;
+    grid-template-columns: 20% 60% 20%;
+    grid-template-rows: 100%;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+      ". illustrationContainer .";
+    grid-area: 2 / 1 / 3 / 2;
+
+    .illustrationContainer {
+      grid-area: illustrationContainer;
+
+      .illustration {
+        align-items: center;
+        text-align: center;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 75%;
+      }
+    }
+  }
+
+  .avatars {
+    position: fixed;
+    bottom: 175px;
+    width: 100%;
+    max-height: 300px;
+    display: grid;
+    grid-template-columns: 40% 20% 40%;
+    grid-template-rows: 100%;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+      "teacher . students";
+    grid-area: 2 / 1 / 3 / 2;
+
+    .teacher {
+      width: ${calculateResponsiveSize(75, 200)};
+      grid-area: teacher;
+    }
+
+    .students {
+      position: absolute;
+      right: 0px;
+      bottom: 0px;
+      display: flex;
+      flex-direction: row;
+      grid-area: students;
+
+      .student {
+        width: ${calculateResponsiveSize(50, 165)};
+      }
+    }
+  }
+
+  .choices {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    grid-template-rows: 100%;
+    gap: 0px 0px;
+    grid-template-areas: 
+      "questions illustrations"; 
+    grid-area: 3 / 1 / 4 / 2; 
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 0;
+    padding: 10px;
+    min-height: 150px;
+    position: fixed;
+    bottom: 0;
+
+    flex-direction: row;
+    justify-content: space-around;
+
+    .questions {
+      button {
+        margin: 0 10px;
+      }
+      grid-area: questions;
+    }
+    .illustrations {
+      grid-area: illustrations;
+
+      .illustration {
+      }
+    }
+  }
 `
 
 export const StyledAlternatives = styled.div`
@@ -73,22 +168,5 @@ export const StyledAlternatives = styled.div`
 
   button {
     margin: 0 10px;
-  }
-`
-
-export const StyledIcons = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: baseline;
-  justify-content: space-around;
-
-  .teacher {
-    width: ${calculateResponsiveSize(150, 400)};
-  }
-  .students {
-    justify-content: flex-end;
-  }
-  .student {
-    width: ${calculateResponsiveSize(69, 200)};
   }
 `
